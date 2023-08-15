b7c78c200bc9eaaa49088e759568e72d1943adb8
@stux <hello@mstdn.social>
2023-08-15 23:57:41 +0200

    Vite update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -21,6 +21,5 @@ export default defineConfig({
                 },
             },
         }),
-        VitePWA({ registerType: 'autoUpdate' }),
     ],
 });
